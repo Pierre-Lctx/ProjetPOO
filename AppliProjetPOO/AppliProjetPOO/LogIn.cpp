7ada3475da5f83cfd597073e2dedcc08e9f0f76a
Pierre-Lctx <pierre.lechatreux@hotmail.fr>
2021-11-23 10:17:58 +0100

    Création Projet VS
    Création du projet visual studio avec le forms de base :
    - Création du forms de connexion

---
+++ AppliProjetPOO/AppliProjetPOO/LogIn.cpp
@@ -0,0 +1,14 @@
+#include "LogIn.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+
+[STAThreadAttribute]
+void Main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	AppliProjetPOO::LogIn monFormulaire;
+	Application::Run(% monFormulaire);
+}
